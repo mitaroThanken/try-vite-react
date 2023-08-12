cb41b335dced115a7e43c07c1e46a060e0699c87
YAMAMI Taro <mitarothanken@gmail.com>
2023-08-12 09:07:43 +0000

    ESLint 周りを更新
    * jsconfig.json を追加
    * @typescript-eslint のルールを type-checked に移行
    * env の違い等を反映させるため overrides を追加

--- sample/.eslintrc.cjs
+++ sample/.eslintrc.cjs
@@ -1,9 +1,19 @@
 module.exports = {
   root: true,
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react-hooks/recommended', 'plugin:storybook/recommended'],
-  ignorePatterns: ['dist', '.eslintrc.cjs', 'storybook-static'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/strict-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:storybook/recommended'
+  ],
+  ignorePatterns: ['dist', 'storybook-static'],
   parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
   plugins: ['react-refresh'],
   rules: {
     'react-refresh/only-export-components': [
@@ -11,4 +21,21 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
+  overrides: [
+    {
+      env: { node: true, es2020: true },
+      files: ["vite.config.ts", ".storybook/main.ts"],
+      parserOptions: {
+        project: ["./tsconfig.node.json"],
+      }
+    },
+    {
+      env: { node: true, es2020: true },
+      extends: ["plugin:@typescript-eslint/disable-type-checked"],
+      files: [".eslintrc.{js,cjs}"],
+      parserOptions: {
+        project: ["./jsconfig.json"]
+      }
+    },
+  ],
 }
