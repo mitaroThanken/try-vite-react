66435fa9b88a00deefe11619ccfc2f9b5a39e3da
YAMAMI Taro <mitarothanken@gmail.com>
2023-08-12 11:30:04 +0000

    [FIX] React 向けのルールが適用しきれてなかった

--- sample/.eslintrc.cjs
+++ sample/.eslintrc.cjs
@@ -1,27 +1,47 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/strict-type-checked',
-    'plugin:@typescript-eslint/stylistic-type-checked',
-    'plugin:storybook/recommended'
   ],
   ignorePatterns: ['dist', 'storybook-static'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
-    project: ["./tsconfig.json"],
-  },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
   },
   overrides: [
+    {
+      env: { browser: true, es2020: true },
+      settings: {
+        react: {
+          version: "detect"
+        }
+      },
+      files: ["src/**/*.{ts,tsx}", ".storybook/preview.ts"],
+      extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/strict-type-checked',
+        'plugin:@typescript-eslint/stylistic-type-checked',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:react-hooks/recommended',
+        'plugin:storybook/recommended'
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        project: ["./tsconfig.json"],
+      },
+      plugins: ['react-refresh'],
+      rules: {
+        'react-refresh/only-export-components': [
+          'warn',
+          { allowConstantExport: true },
+        ],
+      },
+    },
     {
       env: { node: true, es2020: true },
       files: ["vite.config.ts", ".storybook/main.ts"],
